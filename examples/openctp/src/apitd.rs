59d0881f9ee2aa55e8a3e46cb246476968f46ada
pseudo <pseudocodes@gmail.com>
2025-07-14 12:23:03 +0800

    update openctp sample & readme

--- examples/openctp/src/apitd.rs
+++ examples/openctp/src/apitd.rs
@@ -1,5 +1,5 @@
 #![allow(unused_variables)]
-use std::{env::var, path::Path, sync::Arc, thread, time::Duration};
+use std::{sync::Arc, thread, time::Duration};
 
 use ctp2rs::{
     ffi::{gb18030_cstr_i8_to_str, AssignFromString, WrapToString},
@@ -11,20 +11,23 @@ use ctp2rs::{
         CThostFtdcSettlementInfoConfirmField, TraderApi, TraderSpi, THOST_TE_RESUME_TYPE,
     },
 };
+
+use crate::CtpAccountConfig;
+
 pub struct BaseTraderSpi {
     pub tdapi: Arc<TraderApi>,
     pub request_id: i32,
+    pub config: CtpAccountConfig,
 }
 
 impl TraderSpi for BaseTraderSpi {
     fn on_front_connected(&mut self) {
         println!("tdspi.on_front_connected !!!");
         let mut req = CThostFtdcReqAuthenticateField::default();
-        let user_id = var("OPENCTP_USER_ID").unwrap();
         req.BrokerID.assign_from_str("9999");
-        req.UserID.assign_from_str(&user_id);
-        req.AppID.assign_from_str("simnow_client_test");
-        req.AuthCode.assign_from_str("0000000000000000");
+        req.UserID.assign_from_str(&self.config.td_user_id);
+        req.AppID.assign_from_str(&self.config.td_app_id);
+        req.AuthCode.assign_from_str(&self.config.td_auth_code);
 
         self.request_id += 1;
         self.tdapi.req_authenticate(&mut req, self.request_id);
@@ -53,12 +56,9 @@ impl TraderSpi for BaseTraderSpi {
 
         if b_is_last {
             let mut req = CThostFtdcReqUserLoginField::default();
-            let user_id = var("OPENCTP_USER_ID").unwrap();
-            let password = var("OPENCTP_PASS").unwrap();
-
             req.BrokerID.assign_from_str("9999");
-            req.UserID.assign_from_str(&user_id);
-            req.Password.assign_from_str(&password);
+            req.UserID.assign_from_str(&self.config.td_user_id);
+            req.Password.assign_from_str(&self.config.td_password);
 
             self.request_id += 1;
             let ret = self.tdapi.req_user_login(&mut req, self.request_id);
@@ -77,8 +77,7 @@ impl TraderSpi for BaseTraderSpi {
         if b_is_last {
             let mut req = CThostFtdcSettlementInfoConfirmField::default();
             req.BrokerID.assign_from_str("9999");
-            let user_id = var("OPENCTP_USER_ID").unwrap();
-            req.InvestorID.assign_from_str(&user_id);
+            req.InvestorID.assign_from_str(&self.config.td_user_id);
 
             self.request_id += 1;
             let ret = self
@@ -128,36 +127,30 @@ impl TraderSpi for BaseTraderSpi {
     }
 }
 
-pub fn run_td() {
+pub fn run_td(config: CtpAccountConfig) {
     println!("tdapi start here!");
-    let base_dir = var("CARGO_MANIFEST_DIR").unwrap();
-    println!("base_dir: {base_dir}");
-
-    #[cfg(target_os = "macos")]
-    let dynlib_path = "./tts/v6_7_2/mac_arm64/thosttraderapi_se.dylib";
-    #[cfg(target_os = "linux")]
-    let dynlib_path = "./tts/v6_7_2/lin64/thosttraderapi_se.so";
-    #[cfg(target_os = "windows")]
-    let dynlib_path = "./tts/v6_7_2/win64/thosttraderapi_se.dll";
-
-    let dynlib_path = Path::new(&base_dir).join(dynlib_path);
     println!(
         "td dynlib_path: {}",
-        dynlib_path.as_path().to_string_lossy()
+        config.td_dynlib_path.to_string_lossy()
     );
 
-    let tdapi = TraderApi::create_api(dynlib_path.as_path(), "./td_");
+    let tdapi = TraderApi::create_api(&config.td_dynlib_path, "./td_");
     let tdapi = Arc::new(tdapi);
+
+    // 先获取 front_address，避免 move 后的借用问题
+    let front_address = config.td_front_address.clone();
+
     let base_tdspi = BaseTraderSpi {
         tdapi: Arc::clone(&tdapi),
         request_id: 0,
+        config,
     };
     let tdspi_box = Box::new(base_tdspi);
     let tdspi_ptr = Box::into_raw(tdspi_box);
     let tdspi_ptr2 = tdspi_ptr.clone();
     println!("td get_api_version: {}", tdapi.get_api_version());
 
-    tdapi.register_front("tcp://121.37.80.177:20004"); // tts 7x24 td
+    tdapi.register_front(&front_address);
 
     tdapi.register_spi(tdspi_ptr);
     tdapi.subscribe_private_topic(THOST_TE_RESUME_TYPE::THOST_TERT_QUICK);
